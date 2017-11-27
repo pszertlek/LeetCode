4794114e9d86a36c74847bc7f6d2c8f62e0365a6
pszertlek <357426163@qq.com>
2017-11-27 15:29:34 +0800

    init

---
+++ LeetCode/LeetCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCode
+//
+//  Created by apple on 2017/11/27.
+//  Copyright © 2017年 Pszertlek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
