f4397766f84fab07a118ebe4004060a9345d1560
pszertlek <357426163@qq.com>
2018-02-02 17:52:12 +0800

    commit

--- LeetCode/LeetCode/main.swift
+++ LeetCode/LeetCode/main.swift
@@ -8,8 +8,22 @@
 
 import Foundation
 
-let matrix = [2,3,4]
+let matrix = [6,5,5]
 
-print(twoSum(matrix, 6))
+func majorityElement(_ nums: [Int]) -> Int {
+    var vote = 0,count = 0
+    for num in nums {
+        if count == 0 {
+            vote = num
+            count = 1
+        } else if vote == num {
+            count += 1
+        } else {
+            count -= 1
+        }
+    }
+    return vote
+}
+print(majorityElement(matrix))
 
 
